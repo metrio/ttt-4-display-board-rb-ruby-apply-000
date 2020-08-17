989663889e414282b809b4cfcf3319ff509378f3
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-17 19:21:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" ", " " ," ", " " ," ", " ", " ", " ", " "]
+
+def display_board(board)
+  capture_puts{display_board(board)}
+rows[0] = "#{board[0]}|#{board[1]}|#{board[2]}"
+rows[1] = "-----------"
+rows[2] = "#{board[3]}|#{board[4]}|#{board[5]}"
+rows[3] = "-----------"
+rows[4] = "#{board[6]}|#{board[7]}|#{board[8]}"
+puts display_board(board)
+
+
+end
