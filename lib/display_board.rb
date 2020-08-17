d73366f710fd799c4d26484566c84edba4a37ddf
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-17 19:28:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,9 +6,9 @@ board = [" ", " " ," ", " " ,"  ", "  ", " ", " ", " "]
 def display_board(board)
 rows[0] = "#{board[0]} | #{board[1]} | #{board[2]}"
 rows[1] = "-----------"
-rows[2] = "#{board[3]}|#{board[4]}|#{board[5]}"
+rows[2] = "#{board[3]} | #{board[4]} | #{board[5]}"
 rows[3] = "-----------"
-rows[4] = "#{board[6]}|#{board[7]}|#{board[8]}"
+rows[4] = "#{board[6]} | #{board[7]} | #{board[8]}"
 puts display_board(board)
 
 
